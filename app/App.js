f934dc253b0ea487a617522bc9b0748bc8240949
KateMas <e.masyuta@gmail.com>
2017-01-28 09:35:37 +0200

    added form

--- app/App.js
+++ app/App.js
@@ -1,3 +1,21 @@
-/**
- * Created by Екатерина on 27.01.2017.
- */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Form extends React.Component {
+    render() {
+        return (
+            <form method="post" action="">
+                <div className="wrapper">
+                    <input className="js-input" type="text" placeholder="Enter city"/>
+                    <span className="close"></span>
+                </div>
+                <button>Request</button>
+            </form>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Form/>,
+    document.getElementById('app')
+);
