203bfeadb1d30a60d3b296c97d7cf29d571cf142
KateMas <e.masyuta@gmail.com>
2017-01-28 09:38:15 +0200

    added task for js

--- gulpfile.js
+++ gulpfile.js
@@ -14,9 +14,20 @@ gulp.task('styles', function(){
         .pipe(gulp.dest('./public'))
 });
 
+//script
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglifyjs');
+
+gulp.task('scripts', function(){
+    gulp.src(['./node_modules/jquery/dist/jquery.js', './app/js/*.js'])
+        .pipe(concat('script.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./public'))
+});
+
 //watch
 gulp.task('watch', function () {
     gulp.watch('./dev/sass/style.sass', ['styles']);
 });
 
-gulp.task('default', ['styles', 'watch']);
+gulp.task('default', ['styles', 'scripts', 'watch']);
